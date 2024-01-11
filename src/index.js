9c869e865b81ecd0f2ed4c7798584465da30937d
Emily Ingraham <emilyvingraham@gmail.com>
2024-01-11 09:55:38 -0700

    Removed console.log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-    console.log('poem generated');
     new Typewriter('#poem', {
         strings: response.data.answer,
         autoStart: true,
@@ -22,9 +21,6 @@ function generatePoem(event) {
     let poemElement = document.querySelector('#poem');
     poemElement.innerHTML = `<div class="blink">⌛Generating a poem about ${instructionsInput.value}</div>`;
 
-    console.log('Generating Poem');
-    console.log(`Prompt: ${prompt}`);
-    console.log(`Context: ${context}`);
     axios.get(apiUrl).then(displayPoem);
 }
 
